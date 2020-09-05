682fe84d8151fe983c019adb3db0d690a98193f2
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-09-05 18:47:06 +0530

    hospital side

---
+++ client/src/components/Hospital/ImageCompStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "50vh",
+  },
+  text: {
+    fontSize: "40px",
+    color: "#ffffff",
+    letterSpacing: "2px",
+  },
+}));
